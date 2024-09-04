a26878034993ae18989d1ca2b9043027e7e6edff
Alan Zayon <alanzayon72@gmail.com>
2024-09-04 17:59:30 -0300

    deploy

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL
 		console.error("Error connecting to the database:", error);
 	});
 
-	const serviceAccount = {
-		type: process.env.FIREBASE_TYPE,
-		project_id: process.env.FIREBASE_PROJECT_ID,
-		private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-		private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-		client_email: process.env.FIREBASE_CLIENT_EMAIL,
-		client_id: process.env.FIREBASE_CLIENT_ID,
-		auth_uri: process.env.FIREBASE_AUTH_URI,
-		token_uri: process.env.FIREBASE_TOKEN_URI,
-		auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-		client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-	  };
+const serviceAccount = {
+	type: process.env.FIREBASE_TYPE,
+	project_id: process.env.FIREBASE_PROJECT_ID,
+	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+	private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+	client_email: process.env.FIREBASE_CLIENT_EMAIL,
+	client_id: process.env.FIREBASE_CLIENT_ID,
+	auth_uri: process.env.FIREBASE_AUTH_URI,
+	token_uri: process.env.FIREBASE_TOKEN_URI,
+	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+	client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 // Inicialize o Firebase Admin SDK com as credenciais do serviço
 admin.initializeApp({
@@ -39,13 +39,13 @@ const bucket = admin.storage().bucket();
 const app = express();
 
 const corsOptions = {
-	origin: 'http://localhost:5173', // Permite apenas essa origem
+	origin: '*', // Permite apenas essa origem
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	exposedHeaders: ['Authorization-token'], // Expondo o cabeçalho `Authorization-token`
-  };
-  
-  app.use(cors(corsOptions));
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 app.set("views", path.join(__dirname, "../public/views"));
