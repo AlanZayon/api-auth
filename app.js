99c727d5cc37132eb29c4341d563ae948b7d0826
Alan Zayon <alanzayon72@gmail.com>
2024-09-05 12:13:24 -0300

    deploy

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(cors({
 	origin: 'http://localhost:5173', 
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-type'], 
-	exposedHeaders: ['Authorization']
+	exposedHeaders: ['Authorization-token']
   }));
 
 app.use(express.urlencoded({ extended: true }));
