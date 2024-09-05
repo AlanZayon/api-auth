e5971c26e5de8308c94e7a3cb4391d6e36428497
Alan Zayon <alanzayon72@gmail.com>
2024-09-05 14:46:02 -0300

    mailer mod

--- config/mailer.js
+++ config/mailer.js
@@ -14,19 +14,29 @@ oauth2Client.setCredentials({
 	refresh_token: process.env.OAUTH_REFRESH_TOKEN
 });
 
-const accessToken = oauth2Client.getAccessToken();
+// Cria uma função assíncrona para obter o access token
+async function createTransporter() {
+	try {
+		const accessToken = await oauth2Client.getAccessToken();
 
-// Cria um transporte de email com OAuth2
-const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		type: "OAuth2",
-		user: process.env.EMAIL_USER,
-		clientId: process.env.OAUTH_CLIENT_ID,
-		clientSecret: process.env.OAUTH_CLIENT_SECRET,
-		refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-		accessToken: accessToken,
+		// Cria um transporte de email com OAuth2
+		const transporter = nodemailer.createTransport({
+			service: "gmail",
+			auth: {
+				type: "OAuth2",
+				user: process.env.EMAIL_USER,
+				clientId: process.env.OAUTH_CLIENT_ID,
+				clientSecret: process.env.OAUTH_CLIENT_SECRET,
+				refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+				accessToken: accessToken.token, // Acesso ao token do accessToken
+			}
+		});
+
+		return transporter;
+	} catch (error) {
+		console.error("Error creating transporter:", error);
+		throw error; // Lançar o erro para que possa ser tratado por quem chamar a função
 	}
-});
+}
 
-module.exports = transporter;   
+module.exports = createTransporter;
