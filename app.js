6dbbdc5cd23018d981dd723269cee22b20e1a653
Alan Zayon <alanzayon72@gmail.com>
2024-09-13 17:44:32 -0300

    verification token method

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const app = express();
 app.use(cors({
 	origin: ['http://localhost:5173', 'https://site-kong.netlify.app'], 
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'X-Firebase-Token', 'X-JWT-Token'], 
+	allowedHeaders: ['Content-Type', 'X-Firebase-Token', 'X-JWT-Token', 'x-auth-type'], 
 	exposedHeaders: ['Authorization-token']
   }));
 
